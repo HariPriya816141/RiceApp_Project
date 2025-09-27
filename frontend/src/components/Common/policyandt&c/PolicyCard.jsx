7a0782d33892b0db511b9e4d6a135e34a7d6bff3
HariPriya816141 <haripriya816141@gmail.com>
2025-09-27 20:11:04 +0530

    npm run build locally passed

--- frontend/src/components/Common/policyandt&c/PolicyCard.jsx
+++ frontend/src/components/Common/policyandt&c/PolicyCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import grainLeft from "../../assets/grain-left.png";
-import grainRight from "../../assets/grain-right.png";
+// import grainLeft from "../../assets/grain-left.png"
+// import grainRight from "../../assets/grain-right.png";
 import "./policy.css"
 
 const PolicyCard = ({ title, content }) => {
   return (
     <div className="policy-wrapper">
-      <img src={grainLeft} alt="grain left" className="grain grain-left" />
+      {/* <img src={grainLeft} alt="grain left" className="grain grain-left" /> */}
       <div className="policy-card">
         <h2>{title}</h2>
         <div className="policy-content">{content}</div>
       </div>
-      <img src={grainRight} alt="grain right" className="grain grain-right" />
+      {/* <img src={grainRight} alt="grain right" className="grain grain-right" /> */}
     </div>
   );
 };
